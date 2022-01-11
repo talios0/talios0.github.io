4c353a22c05bb0b4e2fd5d6102826b5d2744ffcc
Charles <20057606+talios0@users.noreply.github.com>
2022-01-10 20:10:43 -0500

    Carousel

---
+++ js/carousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.carousel').slick({
+        adaptiveHeight: false,
+        autoplay: true,
+        arrows: false,
+        centerMode: true,
+        dots: false,
+        infinite: true,
+        centerPadding: "0px",
+        rows: 1,
+    });
+});
